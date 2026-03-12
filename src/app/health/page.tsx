1fdf081db93dc2705dd401cd59402d1d3102af64
Tomcy <designsbytomc@gmail.com>
2026-03-13 02:34:29 +0530

    v0: build errors

--- src/app/health/page.tsx
+++ src/app/health/page.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import { HealthView } from "@/features/support/components/health-view";
 
-export default async function Health() {
+export default function Health() {
   return <HealthView />;
 }
